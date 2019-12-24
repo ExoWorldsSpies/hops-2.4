b355d67396139409c3a198404ead31e08a62a9a3
Angelos Tsiaras <aggelostsiaras@gmail.com>
2019-12-24 12:40:27 +0000

    previus version

---
+++ hops/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__version__ = '2.4.0'
+__message__ = 'New feature - Observation planner!'
+
+from .__run__ import run_app
+
+
+def __get_abspath__():
+    import os
+    return os.path.abspath(os.path.dirname(__file__))
